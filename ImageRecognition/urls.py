192763f967118805f3c0f79f393c9bbdc5ec7f18
unknown <spoornartha@gmail.com>
2020-07-02 21:46:07 +0530

    Dynamic Page Changing Enabled

--- ImageRecognition/urls.py
+++ ImageRecognition/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('limeroad', views.limeroad, name='limeroad'),
+    path('zobello', views.zobello, name='zobello')
 ]
--- ImageRecognition/views.py
+++ ImageRecognition/views.py
@@ -6,8 +6,19 @@ from . import forms
 # Imaginary function to handle an uploaded file.
 from .operations import recognize, scrape, scrape_limeroad, scrape_zobello
 
+search_global = ''
 
 def home(request):
+    global search_global
+    output = search_global
+    if 'trunks' in output or 'shorts' in output or 'mini' in output:
+        output = 'short'
+    elif 'shoe' in output:
+        output = 'shoe'
+    search_term = output
+    listings = scrape_limeroad(search_term)
+    context = {'search_term': search_term,
+                'listings': listings}
     if request.method == 'POST':
         form = forms.UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -16,22 +27,89 @@ def home(request):
                 output = 'short'
             elif 'shoe' in output:
                 output = 'shoe'
-            elif 'jersey':
-                output = 'tee'
-            else:
-                output = 'shirt'
             to_search = output.split('_')
             search_term = ''
             for i in to_search:
-                search_term += i 
+                search_term += i
+            search_global = search_term
             listings = scrape(search_term)
-            listings += scrape_limeroad(search_term)
-            listings += scrape_zobello(search_term)
-            return render(request, 'search.html', {'form': form, 'output': output, 'listings': listings})
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'output': output}
+            return render(request, 'search.html', context)
     else:
-    	form = forms.UploadFileForm()
-    return render(request, 'search.html', {'form': form})
+        form = forms.UploadFileForm()
+        context['form'] = form
+    return render(request, 'search.html', context)
 
-# def search(request, query):
-#     
-#     return render(request, 'search.html', {'query': search_term})
+
+def limeroad(request):
+    global search_global
+    output = search_global
+    if 'trunks' in output or 'shorts' in output or 'mini' in output:
+        output = 'short'
+    elif 'shoe' in output:
+        output = 'shoe'
+    search_term = output
+    listings = scrape_limeroad(search_term)
+    context = {'search_term': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            output = recognize(request.FILES['image'])
+            if 'trunks' in output or 'shorts' in output or 'mini' in output:
+                output = 'short'
+            elif 'shoe' in output:
+                output = 'shoe'
+            to_search = output.split('_')
+            search_term = ''
+            for i in to_search:
+                search_term += i
+            search_global = search_term
+            listings = scrape_limeroad(search_term)
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'output': output}
+            return render(request, 'search.html', context)
+    else:
+        form = forms.UploadFileForm()
+        context['form'] = form
+    return render(request, 'search.html', context)
+
+def zobello(request):
+    global search_global
+    output = search_global
+    if 'trunks' in output or 'shorts' in output or 'mini' in output:
+        output = 'short'
+    elif 'shoe' in output:
+        output = 'shoe'
+    search_term = output
+    listings = scrape_limeroad(search_term)
+    context = {'search_term': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            output = recognize(request.FILES['image'])
+            if 'trunks' in output or 'shorts' in output or 'mini' in output:
+                output = 'short'
+            elif 'shoe' in output:
+                output = 'shoe'
+            to_search = output.split('_')
+            search_term = ''
+            for i in to_search:
+                search_term += i
+            search_global = search_term
+            listings = scrape_zobello(search_term)
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'output': output}
+            return render(request, 'search.html', context)
+    else:
+        form = forms.UploadFileForm()
+        context['form'] = form
+    return render(request, 'search.html', context)
+
+
+
