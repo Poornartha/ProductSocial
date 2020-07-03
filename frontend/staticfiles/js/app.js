fef92d767e189e12a466d10b998dbebde4640747
saurav <sauravUppoor@users.noreply.github.com>
2020-07-03 20:13:07 +0530

    UI updates

--- frontend/staticfiles/js/app.js
+++ frontend/staticfiles/js/app.js
@@ -1,4 +1,38 @@
-$(".custom-file-input").on("change", function() {
-    var fileName = $(this).val().split("\\").pop();
-    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-});
+
+/* Sticky Navbar */
+var nav = document.getElementById("navbar");
+var landing = document.getElementById("navbar-landing");
+var sticky;
+if(nav != null) sticky = nav.offsetTop;
+else sticky = landing.offsetTop;
+
+
+window.onscroll = () => {
+    if (window.pageYOffset > sticky) {
+        if(nav != null) nav.classList.add("sticky");
+        else {
+            landing.classList.add("sticky");
+            landing.classList.add("custom-light");
+            landing.classList.remove("custom-dark");
+        }
+    }
+    else {
+        if(nav != null) nav.classList.remove("sticky");
+        else {
+            landing.classList.remove("sticky");
+            landing.classList.add("custom-dark");
+            landing.classList.remove("custom-light");
+        }
+    }
+};
+
+/* Login Signup */
+function gotoSignUp() {
+    document.getElementById("sign-up").style.display = "inline";
+    document.getElementById("login").style.display = "none";
+}
+
+function gotoLogin() {
+    document.getElementById("sign-up").style.display = "none";
+    document.getElementById("login").style.display = "inline";
+}
