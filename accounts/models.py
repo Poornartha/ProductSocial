5e032034cc7e4fdd2818cee575d9b7fe8b796880
Archit Shinde <archits581@gmail.com>
2020-07-05 01:48:32 +0530

    added gender field to customers

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,10 @@ from django.contrib import auth
 #     def __str__(self):
 #         return '{}'.format(self.username)
 
+default_gender = "male"
+
 class Customer(models.Model):
     user = models.OneToOneField(auth.models.User, on_delete=models.CASCADE)
     favourite_color = models.CharField(max_length=100)
     favourite_item = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100, default=default_gender)
