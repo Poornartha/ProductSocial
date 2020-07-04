8a4a08adddaa4ba7bfc849b513fb97b5a8af236e
unknown <spoornartha@gmail.com>
2020-07-04 14:05:29 +0530

    Search Functionality Added

--- ImageRecognition/views.py
+++ ImageRecognition/views.py
@@ -236,4 +236,213 @@ def trending(request):
 
     return render(request, 'trending.html', context)
 
-    
+
+#####################################################################################
+############################ Search Term ############################################
+#####################################################################################
+
+def search_bewakoof(request):
+    global search_global
+    output = search_global
+    search_term = output
+    listings = scrape(search_term)
+    if len(listings) == 0:
+        search_term = search_term.split(' ')[0]
+        listings = scrape(search_term.split(' ')[0])
+    context = {'search': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.GetSearch(request.POST)
+        if form.is_valid():
+            output = form.cleaned_data.get('search')
+            if 'trunk' in output or 'shorts' in output or 'mini' in output:
+                output = 'shorts'
+            elif 'shoe' in output:
+                output = 'shoe'
+            elif 'jersey' in output:
+                output = 'tee'
+            elif 'cardigan' in output:
+                output = 'shirt'
+            search_term = output
+            search_global = search_term
+            listings = scrape(search_term)
+
+            if request.user.is_active and search_term != '':
+                s, created = Search.objects.get_or_create(search_term=search_term)
+                s.users.add(request.user)
+
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'search': search_term}
+            return render(request, 'search-term.html', context)
+    else:
+        form = forms.GetSearch()
+        context['form'] = form
+    return render(request, 'search-term.html', context)
+
+
+def search_limeroad(request):
+    global search_global
+    output = search_global
+    search_term = output
+    listings = scrape_limeroad(search_term)
+    if len(listings) == 0:
+        search_term = search_term.split(' ')[0]
+        listings = scrape_limeroad(search_term.split(' ')[0])
+    context = {'search': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.GetSearch(request.POST)
+        if form.is_valid():
+            output = form.cleaned_data.get('search')
+            if 'trunk' in output or 'shorts' in output or 'mini' in output:
+                output = 'shorts'
+            elif 'shoe' in output:
+                output = 'shoe'
+            elif 'jersey' in output:
+                output = 'tee'
+            elif 'cardigan' in output:
+                output = 'shirt'
+            search_term = output
+            search_global = search_term
+            listings = scrape_limeroad(search_term)
+
+            if request.user.is_active and search_term != '':
+                s, created = Search.objects.get_or_create(search_term=search_term)
+                s.users.add(request.user)
+
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'search': search_term}
+            return render(request, 'search-term.html', context)
+    else:
+        form = forms.GetSearch()
+        context['form'] = form
+    return render(request, 'search-term.html', context)
+
+def search_zobello(request):
+    global search_global
+    output = search_global
+    search_term = output
+    listings = scrape_zobello(search_term)
+    if len(listings) == 0:
+        search_term = search_term.split(' ')[0]
+        listings = scrape_zobello(search_term.split(' ')[0])
+    context = {'search': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.GetSearch(request.POST)
+        if form.is_valid():
+            output = form.cleaned_data.get('search')
+            if 'trunk' in output or 'shorts' in output or 'mini' in output:
+                output = 'shorts'
+            elif 'shoe' in output:
+                output = 'shoe'
+            elif 'jersey' in output:
+                output = 'tee'
+            elif 'cardigan' in output:
+                output = 'shirt'
+            search_term = output
+            search_global = search_term
+            listings = scrape_zobello(search_term)
+
+            if request.user.is_active and search_term != '':
+                s, created = Search.objects.get_or_create(search_term=search_term)
+                s.users.add(request.user)
+
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'search': search_term}
+            return render(request, 'search-term.html', context)
+    else:
+        form = forms.GetSearch()
+        context['form'] = form
+    return render(request, 'search-term.html', context)
+
+def search_random(request):
+    global search_global
+    output = search_global
+    search_term = output
+    listings = scrape(search_term)
+    if len(listings) == 0:
+        search_term = search_term.split(' ')[0]
+        listings = scrape(search_term.split(' ')[0])
+    context = {'search': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.GetSearch(request.POST)
+        if form.is_valid():
+            output = form.cleaned_data.get('search')
+            if 'trunk' in output or 'shorts' in output or 'mini' in output:
+                output = 'shorts'
+            elif 'shoe' in output:
+                output = 'shoe'
+            elif 'jersey' in output:
+                output = 'tee'
+            elif 'cardigan' in output:
+                output = 'shirt'
+            search_term = output
+            search_global = search_term
+            listings = scrape(search_term)
+
+            if request.user.is_active and search_term != '':
+                s, created = Search.objects.get_or_create(search_term=search_term)
+                s.users.add(request.user)
+
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'search': search_term}
+            return render(request, 'search-term.html', context)
+    else:
+        form = forms.GetSearch()
+        context['form'] = form
+    return render(request, 'search-term.html', context)
+
+def search_randomize(request):
+    global search_global
+    output = search_global
+    search_term = output
+    listings_zob = scrape_zobello(search_term)
+    listings_lime = scrape_limeroad(search_term)
+    listings_bewa = scrape(search_term)
+    if output == 'jean' or output=='jersey':
+        listings = listings_lime[:5] + listings_bewa[:5]
+    else:
+        listings = listings_lime[:5] + listings_zob[:5]
+    context = {'search': search_term,
+                'listings': listings}
+    if request.method == 'POST':
+        form = forms.GetSearch(request.POST)
+        if form.is_valid():
+            output = form.cleaned_data.get('search')
+            if 'trunk' in output or 'shorts' in output or 'mini' in output:
+                output = 'shorts'
+            elif 'shoe' in output:
+                output = 'shoe'
+            elif 'jersey' in output:
+                output = 'tee'
+            elif 'cardigan' in output:
+                output = 'shirt'
+            
+            search_term = output
+            search_global = search_term
+
+            if request.user.is_active and search_term != '':
+                s, created = Search.objects.get_or_create(search_term=search_term)
+                s.users.add(request.user)
+
+            listings_zob = scrape_zobello(search_term)
+            listings_lime = scrape_limeroad(search_term)
+            listings_bewa = scrape(search_term)
+            if output == 'jeans' or output == 'jean' or output=='jersey':
+                listings = listings_lime[:5] + listings_bewa[:5]
+            else:
+                listings = listings_lime[:5] + listings_zob[:5]
+            context = {'form': form, 'output': output,
+                        'listings': listings,
+                        'search': search_term}
+            return render(request, 'search-term.html', context)
+    else:
+        form = forms.GetSearch()
+        context['form'] = form
+    return render(request, 'search-term.html', context)
